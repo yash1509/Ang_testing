1a7c973b4bf92fbeb8463f1b00c66b83ffdf8831
yash1509 <yash1509@gmail.com>
2017-05-26 22:22:40 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -15,7 +15,7 @@ angular.module('myApp.config', [])
 
   // double check that the app has been configured before running it and blowing up space and time
   .run(['FBURL', '$timeout', function(FBURL, $timeout) {
-    if( FBURL.match('//friendlychat-4d74f.firebaseio.com') ) {
+    if( FBURL.match('//INSTANCE.firebaseio.com') ) {
       angular.element(document.body).html('<h1>Please configure app/config.js before running!</h1>');
       $timeout(function() {
         angular.element(document.body).removeClass('hide');
